eeed791b1f1f42c93c0b4abca44c95a338349c46
sostaocperu <sorizano@taocperu.com>
2024-11-29 20:32:44 -0500

    subiendo csv

--- app.py
+++ app.py
@@ -9,3 +9,10 @@ import secrets
 #Configurar el cliente de Supabase
 SUPABASE_URL = secrets.SUPABASE_URL
 SUPABASE_KEY = secrets.SUPABASE_KEY
+
+supabase_client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+#Cargar el archivo CSV de datos iniciales
+@st.cache_data
+def cargar_datos():
+    return pd.read_csv('mining_data.csv')
