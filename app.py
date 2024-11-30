fed27fbba246dbf1a9349e7f4b329cdd1553dc28
sostaocperu <sorizano@taocperu.com>
2024-11-29 21:27:18 -0500

    secrets v7.3

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import supabase
 import datetime
 from supabase import create_client
 import json
-import secrets
 import toml
 import os
 
@@ -20,7 +19,7 @@ SUPABASE_KEY = secrets['SUPABASE']['KEY']
 
 supabase_client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-#Cargar el archivo CSV de datos iniciales
+# Cargar el archivo CSV de datos iniciales
 @st.cache_data
 def cargar_datos():
     return pd.read_csv('mining_data.csv')
@@ -32,30 +31,29 @@ def insertar_resultado_prediccion(prediccion_exito):
     }
     supabase_client.table("resultados_prediccion").insert(data).execute()
 
-######## INTERFAZ DE USUARIO CON STREAMLIT ########
-st.title("Modelo Predictivo para proceso minero")
+# Definir la interfaz de usuario con Streamlit
+st.title("Modelo Predictivo para Proceso Minero")
 
-#Cargar datos y mostrarlos
+# Cargar datos y mostrarlos
 st.write("Datos iniciales para entrenamiento:")
 datos = cargar_datos()
 st.dataframe(datos)
 
-#Normalización de los datos
+# Normalización de los datos
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 
 columnas_numericas = ['numero_trabajadores', 'produccion_obtenida', 'consumo_energia', 'calidad_mineral']
-
 datos_normalizados = pd.DataFrame(scaler.fit_transform(datos[columnas_numericas]), columns=columnas_numericas)
 
 datos.update(datos_normalizados)
 
-st.write("Datos normalizados")
+st.write("Datos normalizados:")
 st.dataframe(datos)
 
-#dummy Predictor
+# Dummy Predictor: vamos a simular una predicción simple
 st.subheader("Predicción del proceso")
-if st.button("Predecir Éxito"):
-    exito_predicho = True if datos ['calidad_mineral'].mean() > 7 else False
+if st.button("Predecir Exito"):
+    exito_predicho = True if datos['calidad_mineral'].mean() > 7 else False
     insertar_resultado_prediccion(exito_predicho)
     st.write(f"Resultado de predicción: {'Exitoso' if exito_predicho else 'No Exitoso'}")
